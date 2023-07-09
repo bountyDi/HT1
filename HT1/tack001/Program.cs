f9fbc3a2612932f398bd3ea3f40a9af009dbce01
Diana <dina1405@yandex.ru>
2023-07-09 22:21:16 +0600

    first task

---
+++ HT1/tack001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1>number2)
+{
+    Console.WriteLine(number1);
+}
+    else if (number2>number1)
+{
+    Console.WriteLine(number2);
+} 
+else
+{
+    Console.WriteLine("numbers equal");
+}
