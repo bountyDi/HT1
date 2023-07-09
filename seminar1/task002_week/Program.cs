9711d9d278bccce594bde42decec37b9b5664e17
Diana <dina1405@yandex.ru>
2023-07-09 16:31:16 +0600

    week

---
+++ seminar1/task002_week/Program.cs
@@ -0,0 +1,31 @@
+﻿//Написать прогу, кот. будет выдавать название дня недели по заданному номеру
+Console.WriteLine("Введите номер");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (day == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Thuesday");
+}
+else if (day == 4)
+{
+    Console.WriteLine("Thursday");
+}    
+else if (day == 5)
+{
+    Console.WriteLine("Friday");
+}
+else if (day == 6)
+{
+    Console.WriteLine("Saturday");
+}
+else if (day == 7)
+{
+    Console.WriteLine("Sunday");
+}
