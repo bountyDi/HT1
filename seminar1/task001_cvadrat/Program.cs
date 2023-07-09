53598fa1675f699942b6fad9e38c2f925106d745
Diana <dina1405@yandex.ru>
2023-07-09 16:09:54 +0600

    cvadrat task

---
+++ seminar1/task001_cvadrat/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+// квадрат числа
+int number = 5;
+int result = number*number;
+Console.WriteLine(result);
+*/
+
+// 1. Напишите прогу, кот. на ввод принимает 2 чсила и проверяет, является ли первое число квадратом второго
+Console.WriteLine("Введите первое число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+//== вопрос? равна или нет?
+if(number_1 == number_2*number_2)
+{
+    Console.WriteLine("Первая переменная является квадратом второй");
+}
+else 
+{ 
+    Console.WriteLine("Первая переменная не является квадратом второй");  
+}
+
