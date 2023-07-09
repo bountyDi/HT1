9425148f7e7793daf4d93fd6aad95d3fe4510a6e
Diana <dina1405@yandex.ru>
2023-07-09 17:04:49 +0600

    function

---
+++ seminar1/task003_functionkwadro/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу функции возведения числа в квадрат
+int Power(int number) //метод/функция Power
+{
+  int result = number*number;
+  return result;
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = Power(num);
+Console.WriteLine(result);
