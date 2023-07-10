f76feaaea882d5604b35b69820761706f8a66466
Diana <dina1405@yandex.ru>
2023-07-10 12:06:11 +0600

    task 4

---
+++ HT1/task004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("enter a number");
+int N = Convert.ToInt32(Console.ReadLine());
+N = Math.Abs(N);
+int counter = 0;
+
+while (counter<N)
+{   if(counter%2==0)
+{   
+    Console.Write(counter + " ");
+}
+    counter++;
+}
+
