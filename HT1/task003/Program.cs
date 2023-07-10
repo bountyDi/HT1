a8306ffe744ebb0d8e581501f610d57bc4317535
Diana <dina1405@yandex.ru>
2023-07-10 11:50:43 +0600

    task 3

---
+++ HT1/task003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+Console.WriteLine("even number");
+}
+else 
+{
+Console.WriteLine("odd number");
+}
+
+
